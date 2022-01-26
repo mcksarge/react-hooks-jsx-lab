5bc93804cd5b4f49fac600c5d46795e0975fbcc3
mcksarge <higgins.fred.s@gmail.com>
2022-01-25 16:26:18 -0800

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>West Philadelphia born and raised, on the playground is where I spend most of my days.</p>
+      <img src={image} alt="I made this"></img>
+    </div>
+  );
 }
 
 export default About;
